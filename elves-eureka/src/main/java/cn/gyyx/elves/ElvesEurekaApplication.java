8cd1df28fdc9629bea1d3fcd92a6b92d3f8f4813
fudongfang <fudongfang@gyyx.cn>
2018-05-22 18:34:52 +0800

    elves-eureka

---
+++ elves-eureka/src/main/java/cn/gyyx/elves/ElvesEurekaApplication.java
@@ -0,0 +1,15 @@
+package cn.gyyx.elves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ElvesEurekaApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ElvesEurekaApplication.class, args);
+	}
+
+}
