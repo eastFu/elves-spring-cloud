9c1bc9770f5682dfa253ee42050ccf769d1f57ed
eastFu <492510931@qq.com>
2018-05-25 17:49:46 +0800

    elves-cron update

--- elves-cron/src/main/java/cn/gyyx/elves/cron/controller/CronController.java
+++ elves-cron/src/main/java/cn/gyyx/elves/cron/controller/CronController.java
@@ -1,15 +1,57 @@
 package cn.gyyx.elves.cron.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class CronController {
 
-    @RequestMapping(value = "/createCron" ,method = RequestMethod.POST)
-    public String createCron(){
+    private static final Logger LOG = LoggerFactory.getLogger(CronController.class);
+
+    @RequestMapping(value = "/createCron" )
+    public String createCron() {
         System.out.println("createCron ...");
         return "1731838AEFF5D6E0";
     }
+
+    @RequestMapping(value = "/startCron")
+    public String startCron() {
+        System.out.println("startCron ...");
+        return "1731838AEFF5D6E0";
+    }
+
+    @RequestMapping(value = "/stopCron")
+    public String stopCron() {
+        System.out.println("stopCron ...");
+        return "1731838AEFF5D6E0";
+    }
+
+    @RequestMapping(value = "/deleteCron")
+    public String deleteCron() {
+        System.out.println("deleteCron ...");
+        return "1731838AEFF5D6E0";
+    }
+
+    @RequestMapping(value = "/cronList")
+    public String cronList() {
+        System.out.println("cronList ...");
+        return "1731838AEFF5D6E0";
+    }
+
+    @RequestMapping(value = "/cronDetail")
+    public String cronDetail() {
+        System.out.println("cronDetail ...");
+        return "1731838AEFF5D6E0";
+    }
+
+    @RequestMapping(value = "/cronResult")
+    public String cronResult() {
+        System.out.println("cronResult ...");
+        return "1731838AEFF5D6E0";
+    }
+
+
 }
