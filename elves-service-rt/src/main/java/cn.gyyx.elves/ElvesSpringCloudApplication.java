915ece53253280a4565a90b38ecb1d4f2b3ee29c
eastFu <492510931@qq.com>
2019-03-30 14:03:08 +0800

    elves spring cloud

--- elves-service-rt/src/main/java/cn.gyyx.elves/ElvesSpringCloudApplication.java
+++ elves-service-rt/src/main/java/cn.gyyx.elves/ElvesSpringCloudApplication.java
@@ -2,12 +2,11 @@ package cn.gyyx.elves;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 //@EnableEurekaServer
 @SpringBootApplication
 //@NacosConfigurationProperties(dataId = "example",autoRefreshed = true)
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class ElvesSpringCloudApplication {
 
     public static void main(String[] args) {
