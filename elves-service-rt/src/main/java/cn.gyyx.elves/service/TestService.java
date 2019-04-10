2f1887a7b3334d6145984bdeeb2f7e4d319c00a7
eastFu <492510931@qq.com>
2019-04-10 18:43:29 +0800

    config for elves spring cloud

--- elves-service-rt/src/main/java/cn.gyyx.elves/service/TestService.java
+++ elves-service-rt/src/main/java/cn.gyyx.elves/service/TestService.java
@@ -3,19 +3,25 @@ package cn.gyyx.elves.service;
 import cn.gyyx.elves.util.DateUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
+@RefreshScope
 public class TestService {
 
     private final static Logger LOG = LoggerFactory.getLogger(TestService.class);
 
+    @Value("${foo}")
+    String foo;
+
     @RequestMapping("/test")
     public String sayHello(HttpServletRequest request){
-        String say = DateUtils.currentTimestamp2String("yyyy-MM-dd HH:mm:ss SSS")+": hello "+request.getRemoteAddr()+", i am elves service rt!";
+        String say = DateUtils.currentTimestamp2String("yyyy-MM-dd HH:mm:ss SSS")+": hello "+request.getRemoteAddr()+", i am elves service rt!"+foo;
 //        System.out.println(say);
         return say;
     }
